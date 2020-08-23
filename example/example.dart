9f4df74b2a19a656cff7a8b999ed1bc097a70299
ifehrim <ifehrim@gmail.com>
2020-08-23 11:37:44 +0800

    pdf codec

--- example/example.dart
+++ example/example.dart
@@ -6,11 +6,17 @@ Future<void> main() async {
 
   var byter=Byter('hello-world'.codeUnits);
   print(byter);
-  byter.byte();
-  byter.byte();
-  print(byter);
-  byter.eat(Byter('He'.codeUnits));
 
-  print(utf8.decode(byter.all));
+  byter.byte();
+  byter.byte();
+
+  print(byter);
+
+  byter.nyte();
+  byter.nyte();
+
+  print(byter);
+
+  print(byter.str());
 
 }
--- lib/src/byter.dart
+++ lib/src/byter.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:convert/convert.dart';
 
 ///
@@ -8,62 +11,82 @@ import 'package:convert/convert.dart';
 ///
 
 class Byter {
-  final List<int> _bytes = [];
+  List<int> _bytes = [];
 
   List<int> get all => _bytes;
 
   int get first => _bytes.first;
 
+  int get last => _bytes.last;
+  int _bytePos = 0;
+
   void clear() => _bytes.clear();
 
   @override
-  String toString() => 'Byter{${length}}';
+  String toString() => 'Byter{${length} ${all.sublist(0,min(length,10)).toString().replaceAll(']', length>10?'...]':']')} }';
 
-  String toHexString() => hex.encode(all);
+  String toHexString([int len]) => hex.encode(len==null?all:all.sublist(0,len));
 
   Byter([dynamic input]) {
     _bytes.clear();
-    if (input is List<int>) _bytes.addAll(input);
-    if (input is String) _bytes.addAll(hex.decode(input));
+    if (input is String) input = hex.decode(input);
+    if (input is List<int>) {
+      _bytes.addAll(input);
+    }
   }
 
-  int get length => _bytes.length;
+  int get length => _bytes.length - _bytePos;
 
-  bool get isEmpty => _bytes.isEmpty;
+  bool get isEmpty => length <= 0;
 
-  bool get isNotEmpty => _bytes.isNotEmpty;
+  bool get isNotEmpty => length > 0;
 
   Byter bytes([int len = 1]) {
-    var r = _bytes.sublist(0, len);
-    _bytes.removeRange(0, len);
-    return Byter(r);
+    var l=<int>[];
+    for (var i = 0; i < len; i++) {
+      l.add(byte());
+    }
+    return Byter(l);
   }
 
   int byte() {
     int b;
     try {
-      b = bytes(1)?.first;
+      b = _bytes[_bytePos++];
     } catch (e) {
       b = null;
     }
     return b;
   }
 
+  int nyte() {
+    _bytePos--;
+  }
+
   void add(dynamic b) {
     if (b is int) _bytes.add(b);
     if (b is Byter) addAll(b.all);
   }
-
   void addAll(List<int> os) => os.forEach(add);
+//
+//  void eat(dynamic b) {
+//    if (b is int) _bytes.insert(0, b);
+//    if (b is Byter) eatAll(b.all);
+//  }
+//
+//  void eatAll(List<int> os) => os.reversed.forEach(eat);
 
-  void eat(dynamic b) {
-    if (b is int) _bytes.insert(0, b);
-    if (b is Byter) eatAll(b.all);
+  Byter clone({bool reset = false}) {
+    var n = Byter(all);
+    if (reset) clear();
+    return n;
   }
 
-  void eatAll(List<int> os) => os.reversed.forEach(eat);
-
-  Byter clone() => Byter(all);
-
   int toInt({int radix = 16}) => int.parse(toHexString(), radix: radix);
+
+  String str({bool trim=true}) {
+    var r=StringBuffer();
+    all.forEach(r.writeCharCode);
+    return r.toString();
+  }
 }
